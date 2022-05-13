1206ed5f569b34c4e8225d0b9fed4a1a9b2d4c79
songyaoshun <songyaoshun@gmail.com>
2022-05-13 23:16:25 +0800

    evm链上失败交易,原因查询

---
+++ README.md
@@ -0,0 +1,7 @@
+1. urls:
+    a. r ---> rinkeby
+    b. a ---> arbitrum
+    c. b ---> bsctest
+    d. t ---> teleport testnet
+    e. q ---> teleport qanet
+2. node index.js <urls> <tx_hash>
---
+++ index.js
@@ -0,0 +1,45 @@
+// This is universal, works with Infura -- set provider accordingly
+
+const ethers = require('ethers')
+
+// const getRevertReason = require('eth-revert-reason')
+
+const web3 = new Map([
+    ["r", "https://rinkeby.infura.io/v3/023f2af0f670457d9c4ea9cb524f0810"],
+    ["a", "https://rinkeby.arbitrum.io/rpc"],
+    ["b", "https://data-seed-prebsc-2-s2.binance.org:8545"],
+    ["t", "https://evm-rpc.testnet.teleport.network"],
+    ["q", "https://teleport-localvalidator.qa.davionlabs.com/"],
+  ]);
+
+async function reason() {
+    var args = process.argv.slice(2)
+
+    // console.log(await getRevertReason(args[1], network, blockNumber, provider))
+
+    let url = args[0]
+    console.log('WEB3_URL:', web3.get(url))
+    let provider = new ethers.providers.JsonRpcProvider(web3.get(url))
+
+    let hash = args[1]
+    console.log('tx hash:', hash)
+
+    let tx = await provider.getTransaction(hash)
+    if (!tx) {
+        console.log('tx not found')
+    } else {
+
+        try {
+            let code = await provider.call(tx, tx.blockNumber)
+          } catch (err) {
+            const code = err.data.replace('Reverted ','');
+            // console.log({err});
+            let reason = ethers.utils.toUtf8String('0x' + code.substr(138));
+            console.log('revert reason:', reason);
+          }
+    }
+
+
+}
+
+reason()
